a5b41e5e80bf6ce457a8075c15d0fa337f6cfaf0
Website Administrator <user@example.com>
2026-03-04 15:05:55 +0800

    update

--- resize_favicon.py
+++ resize_favicon.py
@@ -4,10 +4,10 @@ import os
 # 打开原始图片
 img = Image.open('logos/yggdrasill_logo.png')
 
-# 调整尺寸到 64x64
-img_resized = img.resize((64, 64), Image.Resampling.LANCZOS)
+# 调整尺寸到 128x128
+img_resized = img.resize((128, 128), Image.Resampling.LANCZOS)
 
 # 保存为 favicon.ico
-img_resized.save('favicon.ico', format='ICO', sizes=[(64, 64)])
+img_resized.save('favicon.ico', format='ICO', sizes=[(128, 128)])
 
-print('favicon.ico 已调整为 64x64 尺寸')
+print('favicon.ico 已调整为 128x128 尺寸')
